97bda933fc0c11252e548c8bb94d80b7da8fccd6
Isaac Restrick <isaac.restrick@gmail.com>
2025-09-29 17:25:52 -0400

    implemented basic auth

---
+++ app/ui_components/signin.tsx
@@ -0,0 +1,58 @@
+import { Form } from "react-router"
+import { useState } from "react"
+import { authClient } from "../lib/auth-client"
+
+export default function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const signIn = async () => {
+    await authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onRequest: (ctx) => {
+          // show loading state
+          console.log("hello")
+        },
+        onSuccess: (ctx) => {
+          console.log("hi")
+          // redirect to home
+        },
+        onError: (ctx) => {
+          console.log("hey")
+          alert(ctx.error)
+        },
+      },
+    )
+  }
+
+  return (
+    <div className="border border-white mb-4">
+      <h2>
+        Sign In
+      </h2>
+      <Form onSubmit={signIn}>
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+        >
+          Sign In
+        </button>
+      </Form>
+    </div>
+  )
+}
